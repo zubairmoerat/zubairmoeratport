8d89b52cec83693465c5f9572df6dcbe0153d9b2
zubairmoerat <zubairmoeratofficial@gmail.com>
2024-04-18 11:03:33 +0200

    finalised

--- src/components/AboutComp.vue
+++ src/components/AboutComp.vue
@@ -2,16 +2,24 @@
     <div id="about" class="container-fluid">
         <h1 class="text-center">About Me</h1>
         <div class="row">
-            <div class="col d-flex justify-content-center">
+            <div class="col">
                 <img src="https://iili.io/JN87gpf.jpg" alt="me" class="img fluid rounded">
             </div>
             <div class="col">
-                <p class="text-start">
-                    I'm a dynamic individual with a passion for a well-rounded lifestyle. I find joy in staying active and fit, making frequent visits to the gym to engage in various workouts.
-                    The love for the outdoors is another facet of my life, as hiking, biking, and exploring nature provides a perfect balance to the tech-driven world.On the intellectual side, I'm deeply immersed in the fields of engineering and software development.
-                    With a keen interest in problem-solving and innovation, I enjoy tackling challenges both in the physical realm through engineering projects and in the virtual world through software development.
-                    This combination of a strong body, an adventurous spirit, and a sharp mind defines my multifaceted and fulfilling lifestyle.
-                </p>
+                <div class="para">
+                    <p class="text-center">
+                        I'm a dynamic individual with a passion for a well-rounded lifestyle. I find joy in staying
+                        active and fit, making frequent visits to the gym to engage in various workouts.
+                        The love for the outdoors is another facet of my life, as hiking, biking, and exploring nature
+                        provides a perfect balance to the tech-driven world.On the intellectual side, I'm deeply
+                        immersed in the fields of engineering and software development.
+                        With a keen interest in problem-solving and innovation, I enjoy tackling challenges both in the
+                        physical realm through engineering projects and in the virtual world through software
+                        development.
+                        This combination of a strong body, an adventurous spirit, and a sharp mind defines my
+                        multifaceted and fulfilling lifestyle.
+                    </p>
+                </div>
             </div>
         </div>
     </div>
@@ -27,11 +35,15 @@
 #about{
     margin-bottom: 100px;
 }
-.row{
+/* .row{
     padding-top: 50px;
     display: flex;
     flex-direction: row;
     align-items: center;
+} */
+.para{
+    width: 65%;
+    margin: auto;
 }
 
 h1{
